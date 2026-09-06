04aeff1e645b3741cb026db1806c1ab21bdd9891
jbarszczewski <jakub.barszczewski@outlook.com>
2026-09-06 15:14:05 +0200

    Release 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.jbarszczewski.habits"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
